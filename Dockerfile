2c6fec97b0c151fdada8f7abd30bf94c01a05eed
fandersondeuna <fanderson@deuna.com>
2024-10-14 11:52:56 -0600

    4

--- Dockerfile
+++ Dockerfile
@@ -1,23 +1,23 @@
-# Usa la imagen de Go oficial
+# Usa la imagen de Go
 FROM golang:1.20
 
 # Establece el directorio de trabajo
 WORKDIR /app
 
-# Copia los archivos go.mod y go.sum
+# Copia el archivo go.mod y go.sum
 COPY go.mod go.sum ./
 
 # Descarga las dependencias
 RUN go mod download
 
-# Copia el resto de los archivos de tu aplicación
+# Copia el resto del código
 COPY . .
 
-# Compila el proyecto
-RUN go build -o notifications-server .
+# Compila el código
+RUN go build -o notifications-server
 
-# Expone el puerto
+# Expone el puerto que usará la aplicación
 EXPOSE 8080
 
-# Comando para ejecutar tu aplicación
+# Comando para ejecutar la aplicación
 CMD ["./notifications-server"]
--- main.go
+++ main.go
@@ -1,52 +1,21 @@
 package main
 
 import (
-	"encoding/xml"
-	"fmt"
+	"log"
 	"net/http"
+	"os"
 )
 
-type PaymentNotification struct {
-	XMLName      xml.Name `xml:"paymentService"`
-	MerchantCode string   `xml:"merchantCode,attr"`
-	Notify       Notify   `xml:"notify"`
-}
-
-type Notify struct {
-	OrderStatusEvent OrderStatusEvent `xml:"orderStatusEvent"`
-}
-
-type OrderStatusEvent struct {
-	OrderCode string  `xml:"orderCode,attr"`
-	Payment   Payment `xml:"payment"`
-}
-
-type Payment struct {
-	PaymentMethod string `xml:"paymentMethod"`
-	Amount        Amount `xml:"amount"`
-}
-
-type Amount struct {
-	Value        int    `xml:"value,attr"`
-	CurrencyCode string `xml:"currencyCode,attr"`
-}
-
-func notificationHandler(w http.ResponseWriter, r *http.Request) {
-	var notification PaymentNotification
-	if err := xml.NewDecoder(r.Body).Decode(&notification); err != nil {
-		http.Error(w, "Invalid XML", http.StatusBadRequest)
-		return
-	}
-	fmt.Fprintf(w, "Received notification for order: %s, amount: %d %s",
-		notification.Notify.OrderStatusEvent.OrderCode,
-		notification.Notify.OrderStatusEvent.Payment.Amount.Value,
-		notification.Notify.OrderStatusEvent.Payment.Amount.CurrencyCode)
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Server is running"))
 }
 
 func main() {
-	http.HandleFunc("/notifications", notificationHandler)
-	fmt.Println("Server is listening on port 8080")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		fmt.Println("Failed to start server:", err)
+	port := os.Getenv("PORT") // Fly.io establece el puerto en la variable de entorno PORT
+	if port == "" {
+		port = "8080" // Usar 8080 como puerto por defecto
 	}
+	http.HandleFunc("/", handler)
+	log.Printf("Server is listening on port %s", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
